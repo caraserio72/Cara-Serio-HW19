e64f672575acdf991fb1fb9d187010ddec1862e0
caraserio72 <caraserio72@users.noreply.github.com>
2021-01-06 20:17:54 -0800

    jQuery & CSS Updates to Button Interactions

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,58 @@
-var helloWorld = 'Hello World!';
+// var helloWorld = 'Hello World!';
+
+// $(".primary-nav").hover(function(){
+//     $(this).css({
+//         "font-size": "24px",
+//         "color": "red"
+//     });
+//     }, function(){
+//     $(this).css({
+//         "font-size": "18px",
+//     });
+
+// $(".primary-nav").hover(function(){
+//     $(this).css("font-size","24px");
+//     });
+
+// $(".primary-nav").hover(function(){
+//     $(this).css({
+//         "font-size":"24px",
+//         "font-weight": "bolder"
+//     });
+//     });
+
+// On Hover
+$(".primary-nav").hover(function(){
+    $(this).css({
+        "font-size": "22px",
+        "font-weight": "bolder",
+        "text-decoration": "underline"
+    });
+    }, function(){
+    $(this).css({
+        "font-size": "18px",
+        "font-weight": "regular",
+        "text-decoration": "none"
+    });
+  });
+
+// On Click
+// $(".primary-nav").mousedown(function(){
+//     $(this).css({
+//         "background-color": "#F2CDC3",
+//         "font-size": "24px",
+//         "font-weight": "bolder",
+//         "text-decoration": "underline"
+//     });
+//     }, function(){
+//     $(this).css({
+//         "background-color": "#062559",
+//         "font-size": "18px",
+//         "font-weight": "regular"
+//     });
+//   });
+
+
+$(".button-design").on("click", function(){
+    console.log("button clicked!");
+});
